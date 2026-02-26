761b7fd055c8350ca0626371c5c6fab27cd44523
KaipaUday <117348546+KaipaUday@users.noreply.github.com>
2026-02-26 15:15:42 +0100

    playwright config updated

--- portfolio-frontend/playwright.config.ts
+++ portfolio-frontend/playwright.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testDir: 'tests/e2e'
+	testDir: 'tests/e2e',
+	use: {
+		screenshot : "only-on-failure",
+		trace :"retain-on-failure",
+		video :"retain-on-failure",
+	}
 });
